eb3b35ae6fbd622f31a7b115ccf3f27ed7aeac77
atishoff012803 <160933920+MAKSVOID28032001@users.noreply.github.com>
2024-02-23 02:54:07 +0530

    Final Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Head from "./Components/Header/Head";
+import Form from "./Components/Form/Form";
+
+const App = () => {
+  return (
+    <>
+      <Head />
+      <Form />
+    </>
+  );
+}
+export default App;
